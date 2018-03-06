32eb7b51dc0074654b4df055d6b9ae7a7fbfd6dd
AlijaDelic <aljo_delic007@hotmail.com>
2018-03-06 16:44:54 +0100

    done

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 	
 	var texts = new Array();
 	
 	tempText = JSON.parse(localStorage.getItem("text"));
-	if(tempText != null){
+	
+	if (tempText != null) {
+		
 		texts = tempText;
 	}
 	
-	for(var i=0; i<texts.length;i++){
-		$("table").append("<tr><td class='first'>"+texts[i]+"</td>"+
-		"<td class='second'><input type='checkbox'></td>"+
-		"<td class='third'><i class='material-icons'>delete</i></td>"+
-		"</tr>");
+	for (var i = 0; i < texts.length; i++) {
+		
+		$("table").append("<tr><td class='first'>" + texts[i] + "</td>" +
+		"<td class='second'><input type='checkbox'></td>" +
+		"<td class='third'><i class='material-icons'>delete</i></td>" +
+		"</tr>" );
 	}
-	
-	
-    $("#add_new").on("click", function() {
-        var text = $("#type_in").val();
+
+
+	$("#add_new").on("click", function () {
+		
+		var text = $("#type_in").val();
 		texts.push(text);
-  		localStorage.setItem("text",JSON.stringify(texts));
-		
-		$("table").append("<tr><td class='first'>"+text+"</td>"+
-		"<td class='second'><input type='checkbox'></td>"+
-		"<td class='third'><i class='material-icons'>delete</i></td>"+
+		localStorage.setItem("text", JSON.stringify(texts));
+		$("table").append("<tr><td class='first'>" + text + "</td>" +
+		"<td class='second'><input type='checkbox'></td>" +
+		"<td class='third'><i class='material-icons'>delete</i></td>" +
 		"</tr>");
+	});
+	
+
+	$("body").on("click", ".third", function () {
 		
-    });
-	
-	
-	$("body").on("click",".third", function(){
-		var text = $(this).parent().find(".first").val();
+		var t=$(this).parent().find(".first").text();
+		var d= texts.indexOf(t);
+		texts.splice(d,1);
+		console.log(texts);
 		$(this).parent().hide();
-		var index=texts.indexOf(text);
-		texts.splice(index, 1);
+		localStorage.setItem("text", JSON.stringify(texts));
 	});
 		
 	
-	$("body").on("change","input[type=checkbox]",function(){
+	$("body").on("change", "input[type=checkbox]", function () {
+		
 		 if ($(this).prop("checked")) {
-			 $(this).parent().parent().addClass('checked')
+			 $(this).parent().parent().addClass('checked');
 		}else{
-			 $(this).parent().parent().removeClass('checked')
-		} 
+			 $(this).parent().parent().removeClass('checked');
+		}
 	});
-	$("body").on("click","#add_new", function(){
+	
+	
+	$("body").on("click", "#add_new", function () {
 		$("#type_in").val("");
 	});
-	});
-
+});
