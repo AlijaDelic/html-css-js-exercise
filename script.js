6d235fda1d10b3583b965bbcc138384006af8728
AlijaDelic <aljo_delic007@hotmail.com>
2018-03-06 22:43:11 +0100

    done

--- script.js
+++ script.js
@@ -1,57 +1,90 @@
 $(document).ready(function () {
 	
+	//array for saving data's
 	var texts = new Array();
 	
+	//checking is local storage null
 	tempText = JSON.parse(localStorage.getItem("text"));
-	
 	if (tempText != null) {
-		
 		texts = tempText;
+	};
+	
+	//function for finding index of array
+	function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+       }
+    }
+    return -1;
 	}
 	
+	//showing the results from localstorage
 	for (var i = 0; i < texts.length; i++) {
 		
-		$("table").append("<tr><td class='first'>" + texts[i] + "</td>" +
-		"<td class='second'><input type='checkbox'></td>" +
+		var isChecked;
+		
+		if(texts[i]['checked']){
+			isChecked = "checked";
+		}else{
+			isChecked = "";
+		}
+		
+		$("table").append("<tr class='"+isChecked+"'><td class='first'>" + texts[i]['value']+ "</td>" +
+		"<td class='second'><input type='checkbox'"+ isChecked+"></td>" +
 		"<td class='third'><i class='material-icons'>delete</i></td>" +
 		"</tr>" );
 	}
 
-
+	//adding items to to-do list
 	$("#add_new").on("click", function () {
-		
-		var text = $("#type_in").val();
-		texts.push(text);
-		localStorage.setItem("text", JSON.stringify(texts));
-		$("table").append("<tr><td class='first'>" + text + "</td>" +
-		"<td class='second'><input type='checkbox'></td>" +
-		"<td class='third'><i class='material-icons'>delete</i></td>" +
-		"</tr>");
+		text = $("#type_in").val();
+		text = $.trim(text);
+	
+		if(text != ""){
+			
+			var element = new Object();
+			element['value'] = text;
+			element['checked'];
+			texts.push(element);
+			localStorage.setItem("text", JSON.stringify(texts));
+			
+			$("table").append("<tr><td class='first'>" + text + "</td>" +
+			"<td class='second'><input type='checkbox'"+ isChecked +"></td>" +
+			"<td class='third'><i class='material-icons'>delete</i></td>" +
+			"</tr>");		
+		}	
 	});
 	
-
+	//deleting from list
 	$("body").on("click", ".third", function () {
 		
-		var t=$(this).parent().find(".first").text();
-		var d= texts.indexOf(t);
-		texts.splice(d,1);
-		console.log(texts);
+		var textToCompare = $(this).parent().find(".first").text();
+		var index = findWithAttr(texts,'value',textToCompare);
+		texts.splice(index,1);
 		$(this).parent().hide();
 		localStorage.setItem("text", JSON.stringify(texts));
 	});
 		
-	
+	//adding a class on checked items
 	$("body").on("change", "input[type=checkbox]", function () {
-		
-		 if ($(this).prop("checked")) {
-			 $(this).parent().parent().addClass('checked');
+		var textToCompare = $(this).parent().parent().find(".first").text();
+		var index = findWithAttr(texts,'value',textToCompare);
+	
+		if ($(this).prop("checked")) {
+			$(this).parent().parent().addClass('checked');
+			texts[index]['checked'] = true;
+			localStorage.setItem("text", JSON.stringify(texts));
 		}else{
-			 $(this).parent().parent().removeClass('checked');
+			$(this).parent().parent().removeClass('checked');
+			texts[index]['checked'] = false;
+			localStorage.setItem("text", JSON.stringify(texts));
 		}
 	});
 	
-	
+	//clear input field after adding new item
 	$("body").on("click", "#add_new", function () {
 		$("#type_in").val("");
 	});
+	
 });
